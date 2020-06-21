05d5c735a1e7bc5ad2f036fd8525ed46b0fb2e8c
Reyof <rmb>
2020-06-21 09:05:42 +0300

    validation update

--- validation.js
+++ validation.js
@@ -11,7 +11,17 @@ const signing = require('./signing');
  *   - have been modified since signing
  */
 const isValidTransaction = transaction => {
-  // Enter your solution here
+  if (transaction.amount < 0) {
+    return false;
+  }
+ 
+
+  const toSign = transaction.source
+    + transaction.recipient
+    + transaction.amount;
+
+  return signing.verify(transaction.source, toSign, transaction.signature);
+
 
 };
 
@@ -22,7 +32,15 @@ const isValidTransaction = transaction => {
  *   - they contain any invalid transactions
  */
 const isValidBlock = block => {
-  // Your code here
+  const transactionString = block.transactions.map(t => t.signature).join('');
+  const toHash = block.previousHash + transactionString + block.nonce;
+
+  if (block.hash !== createHash('sha512').update(toHash).digest('hex')) {
+    return false;
+  }
+  
+
+  return block.transactions.every(isValidTransaction);
 
 };
 
@@ -37,7 +55,26 @@ const isValidBlock = block => {
  *   - contains any invalid transactions
  */
 const isValidChain = blockchain => {
+  const { blocks } = blockchain;
+
+  if (blocks[0].previousHash !== null) {
+    return false;
+  }
   // Your code here
+
+  if (blocks.slice(1).some((b, i) => b.previousHash !== blocks[i].hash)) {
+    return false;
+  }
+
+  if (blocks.some(b => !isValidBlock(b))) {
+    return false;
+  }
+
+  return blocks
+    .map(b => b.transactions)
+    .reduce((flat, txns) => flat.concat(txns), [])
+    .every(isValidTransaction);
+
 
 };
 
@@ -47,7 +84,7 @@ const isValidChain = blockchain => {
  * (in theory) make the blockchain fail later validation checks;
  */
 const breakChain = blockchain => {
-  // Your code here
+  blockchain.blocks[1].transactions[0].amount = 1000000000;
 
 };
 
